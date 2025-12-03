db4772b7064946aa0d4508432b36f9fa40531a20
Anson Cheng <Anson.12666@icloud.com>
2025-12-03 17:11:46 +0800

    chore: update chunk

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,24 @@ export default defineConfig({
   plugins: [
     react(),
     legacy(),
-    chunkSplitPlugin({ strategy: 'default' }),
+    chunkSplitPlugin({
+      strategy: 'default',
+      // Group big/vendor libs to keep entry chunks smaller and improve caching.
+      customSplitting: {
+        'react-vendor': [
+          /node_modules\/react/,
+          /node_modules\/react-dom/,
+          /node_modules\/react-router/,
+          /node_modules\/react-router-dom/,
+          /node_modules\/@remix-run/,
+        ],
+        'ionic-vendor': [/node_modules\/@ionic/, /node_modules\/ionicons/],
+        'capacitor-vendor': [/node_modules\/@capacitor/],
+        'i18n-vendor': [/node_modules\/i18next/, /node_modules\/react-i18next/],
+        'sentry-vendor': [/node_modules\/@sentry/],
+        'data-vendor': [/node_modules\/axios/, /node_modules\/redux/, /node_modules\/react-redux/],
+      },
+    }),
     VitePWA({ registerType: 'autoUpdate' }),
   ],
   resolve: {
